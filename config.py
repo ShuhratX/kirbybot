bee613fa9a541ec8f49d2e90cca33c9042d659d0
ShuhratX <shuhratxayitov@gmail.com>
2026-04-02 11:24:37 +0500

    bugfix commit

--- config.py
+++ config.py
@@ -3,9 +3,9 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-BOT_TOKEN: str = os.getenv("BOT_TOKEN", "YOUR_BOT_TOKEN")
+BOT_TOKEN: str = os.getenv("BOT_TOKEN", "8688079137:AAGjbWv_5mD7z9ddlrH0em2fE-CGV4gKri4")
 GROUP_ID: int = int(os.getenv("GROUP_ID", "-1001234567890"))
-WEBAPP_URL: str = os.getenv("WEBAPP_URL", "https://yourdomain.com")
+WEBAPP_URL: str = os.getenv("WEBAPP_URL", "https://kirbybot.vercel.app/")
 ADMIN_IDS: list[int] = [
     int(x) for x in os.getenv("ADMIN_IDS", "143249567").split(",") if x.strip()
 ]
